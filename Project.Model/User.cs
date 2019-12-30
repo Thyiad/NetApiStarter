dc6207691864c4b5ade71de90e2fcf61b0e7047f
Thyiad <1520583107@qq.com>
2019-12-30 19:17:07 +0800

    一些修订

--- Project.Model/User.cs
+++ Project.Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace Project.Model
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 #### 介绍
 基于netcore 3.1 的api模板项目，其中包含swagger文档，jwt权限验证，模型验证，日志，ioc，appEvent，分块上传等等现成的功能，帮你快速开始api的构建
 
+### watch
+dotnet watch --project ./Project.Api/Project.Api.csproj run
+
+### run
+dotnet run --project ./Project.Api/Project.Api.csproj
