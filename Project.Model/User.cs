63e509736de0eb073cc4228f74be814172246261
Thyiad <1520583107@qq.com>
2019-12-31 10:27:08 +0800

    add key and guid for id

--- Project.Model/User.cs
+++ Project.Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,7 @@ namespace Project.Model
 {
     public class User
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
